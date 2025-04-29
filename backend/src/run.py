03efe371e721eab1d2fdabb1ad541522d91ecffa
NicholasTanz <nicholastanzillo@gmail.com>
2025-04-29 11:41:59 -0400

    ignore

--- backend/src/run.py
+++ backend/src/run.py
@@ -18,4 +18,4 @@ with app.app_context():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True, host=os.getenv("FLASK_RUN_HOST"), port=os.getenv("FLASK_RUN_PORT"))
+    app.run(debug=True, host=os.getenv("FLASK_RUN_HOST"), port=os.getenv("FLASK_RUN_PORT")) #nosec: B201
