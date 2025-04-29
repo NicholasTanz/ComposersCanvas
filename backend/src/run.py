efececadbb27d03e7fc8116a22295e8bed73ffa3
NicholasTanz <nicholastanzillo@gmail.com>
2025-04-29 11:38:07 -0400

    basic limiter

--- backend/src/run.py
+++ backend/src/run.py
@@ -2,7 +2,16 @@ from .__init__ import create_app
 from .models import db
 import os
 
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 app = create_app()
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["275 per minute"],
+)
 
 with app.app_context():
     db.create_all()
